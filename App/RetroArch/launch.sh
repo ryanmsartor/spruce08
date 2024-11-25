6079b1482353a09d187b74a520057a2ce8311355
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-11-24 22:01:23 -0600

    simplify apps to just 4 (always shown), plus firmware update if available

--- App/RetroArch/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo $0 $*
-progdir=`dirname "$0"`
-cd /mnt/SDCARD/RetroArch/
-HOME=/mnt/SDCARD/RetroArch/ $progdir/ra32.miyoo -v
-. /mnt/SDCARD/spruce/scripts/helperFunctions.sh
-auto_regen_tmp_update
